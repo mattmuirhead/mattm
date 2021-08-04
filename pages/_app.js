35d7407ef9db3dd6a7d7811226198b6c773fd34d
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2021-08-04 19:20:32 +0100

    setup global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 import theme from '../components/theme'
+import GlobalStyle from '../components/GlobalStyle'
+import Head from 'next/head'
 
 const app = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Matt Muirhead</title>
+        <meta name="description" content="Matt Muirhead - Portfolio Site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   )
